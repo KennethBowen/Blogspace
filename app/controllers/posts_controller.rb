3d1bb3b9088aa27375ba24301051e6a9e33be76e
Kenneth Bowen <bowenk479@gmail.com>
2016-04-12 16:19:07 -0700

    add devise users

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,54 +1,52 @@
 class PostsController < ApplicationController
+	before_action :authenticate_user!, except: [:index, :show]
 
-  def index
-    @posts = Post.all.order('created_at DESC')
-  end
+	def index
+		@posts = Post.all.order('created_at DESC')
+	end
 
-  def new
-    @post = Post.new
-  end
+	def new
+		@post = Post.new
+	end
 
-  def create
-    @post = Post.new(post_params)
+	def create
+		@post = Post.new(post_params)
 
-    if @post.save
-      redirect_to @post
-    else
-      render 'new'
-    end
-  end
+		if @post.save
+			redirect_to @post
+		else
+			render 'new'
+		end
+	end
 
-  def show
-    @post = Post.find(params[:id])
-  end
+	def show
+		@post = Post.find(params[:id])
+	end
 
-  def edit
-    @post = Post.find(params[:id])
-  end
+	def edit
+		@post = Post.find(params[:id])
+	end
 
-  def update
-    @post = Post.find(params[:id])
+	def update
+		@post = Post.find(params[:id])
 
-    if @post.update(params[:post].permit(:title, :body))
-      redirect_to @post
-    else
-      render 'edit'
-    end
-  end
+		if @post.update(params[:post].permit(:title, :body))
+			redirect_to @post
+		else
+			render 'edit'
+		end
+	end
 
-  def destroy
-    @post = Post.find(params[:id])
-    @post.destroy
+	def destroy
+		@post = Post.find(params[:id])
+		@post.destroy
 
-    redirect_to root_path
-  end
+		redirect_to root_path
+	end
 
-  private
+	private
 
-  def post_params
-    params.require(:post).permit(:title, :body)
-  end
-
-  def find_post
-  end
+		def post_params
+			params.require(:post).permit(:title, :body)
+		end
 end
